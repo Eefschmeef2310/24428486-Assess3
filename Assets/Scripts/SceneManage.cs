36cd2c52652ac804754fa3d3ad73d6d6dcb53bce
Eefschmeef2310 <ethan.lucasawesome@gmail.com>
2022-09-29 22:57:31 +1000

    Added most things to the start screen
    Still need to add the animated border and make everything look pretty. Apart from that, it's done.

---
+++ Assets/Scripts/SceneManage.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManage : MonoBehaviour
+{
+    public void Level1()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Level2()
+    {
+        Debug.Log("On to Level 2!");
+    }
+}
