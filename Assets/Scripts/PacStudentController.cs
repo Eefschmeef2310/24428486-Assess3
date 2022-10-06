3cb5333e2e09b040e0f6b54e4e96d0a3b5a1ef43
EthanLucas <ethan.lucasawesome@gmail.com>
2022-10-06 22:20:35 +1100

    Fixed Movement and animation
    Particle Effect and sound to go!

--- Assets/Scripts/PacStudentController.cs
+++ Assets/Scripts/PacStudentController.cs
@@ -6,14 +6,16 @@ using UnityEngine.Tilemaps;
 public class PacStudentController : MonoBehaviour
 {
     Vector3Int lastInput = Vector3Int.right; //Vector3.*direction* will be used.
+    Vector3Int movingDirection = Vector3Int.right;
     public Tilemap tilemap;
+    public Animator animator;
 
-    Vector3 target;
+    Vector3 currentInput;
     public float speed;
 
     void Start()
     {
-        target = transform.position + lastInput;
+        currentInput = transform.position + lastInput;
     }
 
     void Update()
@@ -40,16 +42,45 @@ public class PacStudentController : MonoBehaviour
             //Debug.Log("D");
         }
 
-        //when the player reaches the next tile, check if lastInput is a vlid tile to move to. If so, update target, so the player moves there
-        if(transform.position == target)
+        //when the player reaches the next tile, check if lastInput is a valid tile to move to. If so, update target, so the player moves there
+        if(transform.position == currentInput)
         {
-            Vector3Int nextPos = new Vector3Int((int)target.x + lastInput.x, (int)target.y + lastInput.y);
-            Debug.Log(nextPos);
+            Vector3Int nextPos = new Vector3Int((int)(currentInput.x - 0.5f) + lastInput.x, (int)(currentInput.y - 0.5f) + lastInput.y);
+            //Debug.Log(nextPos);
             if(tilemap.GetTile(nextPos) == null || tilemap.GetTile(nextPos).name == "Pellet" || tilemap.GetTile(nextPos).name == "PowerPellet") //tile in direction is valid
             {
-                target += lastInput;
+                currentInput += lastInput;
+                movingDirection = lastInput;
+            }
+            else
+            {
+                nextPos = new Vector3Int((int)(currentInput.x - 0.5f) + movingDirection.x, (int)(currentInput.y - 0.5f) + movingDirection.y);
+                if(tilemap.GetTile(nextPos) == null || tilemap.GetTile(nextPos).name == "Pellet" || tilemap.GetTile(nextPos).name == "PowerPellet") //tile in direction is valid
+                {
+                    currentInput += movingDirection;
+                }
             }
         }
-        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        else
+        {
+            if(movingDirection == Vector3Int.up)
+            {
+                animator.Play("Up", 1);
+            }
+            if(movingDirection == Vector3Int.left)
+            {
+                animator.Play("Left", 1);
+            }
+            if(movingDirection == Vector3Int.down)
+            {
+                animator.Play("Down", 1);
+            }
+            if(movingDirection == Vector3Int.right)
+            {
+                animator.Play("Right", 1);
+            }
+            transform.position = Vector3.MoveTowards(transform.position, currentInput, speed * Time.deltaTime);
+        }
+        //animator.Play("Right", 1);
     }
 }
